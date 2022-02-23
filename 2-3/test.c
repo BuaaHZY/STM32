b1a9103a4fab3008b8abaa790a14085eb9074aaa
BuaaHZY <100255960+BuaaHZY@users.noreply.github.com>
2022-02-23 12:31:33 +0800

    Add files via upload

---
+++ 2-3/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+int main()
+{
+	int c;
+
+	c = add(2, 3);
+
+	printf("%d\n", c);
+
+	printf("hellow world");
+
+	return 0;
+}
